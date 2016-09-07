44c9e8135353fa44992e241bed9474b703382dae
Kurt A. O'Hearn <ohearnku@cse.msu.edu>
2016-09-07 11:13:49 -0400

    sPuReMD: fix incorrect thread parallelism inside bonded force routines. Update default value of CFLAGS in autotools.

--- sPuReMD/src/three_body_interactions.c
+++ sPuReMD/src/three_body_interactions.c
@@ -30,7 +30,7 @@
 void Calculate_Theta( rvec dvec_ji, real d_ji, rvec dvec_jk, real d_jk,
                       real *theta, real *cos_theta )
 {
-    (*cos_theta) = Dot( dvec_ji, dvec_jk, 3 ) / ( d_ji * d_jk );
+    (*cos_theta) = rvec_Dot( dvec_ji, dvec_jk ) / ( d_ji * d_jk );
     if ( *cos_theta > 1. ) *cos_theta  = 1.0;
     if ( *cos_theta < -1. ) *cos_theta  = -1.0;
 
@@ -48,7 +48,7 @@ void Calculate_dCos_Theta( rvec dvec_ji, real d_ji, rvec dvec_jk, real d_jk,
     real sqr_d_jk   = SQR(d_jk);
     real inv_dists  = 1.0 / (d_ji * d_jk);
     real inv_dists3 = POW( inv_dists, 3 );
-    real dot_dvecs  = Dot( dvec_ji, dvec_jk, 3 );
+    real dot_dvecs  = rvec_Dot( dvec_ji, dvec_jk );
     real Cdot_inv3  = dot_dvecs * inv_dists3;
 
     for ( t = 0; t < 3; ++t )
