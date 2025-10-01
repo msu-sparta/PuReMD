60a446d277f54c46e5b68030c4d496676529e717
Kurt A. O'Hearn <1138669+ohearnk@users.noreply.github.com>
2025-10-01 15:59:00 -0400

    Remove outdated files.

--- environ/serial.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#To run single cpu executable
-spuremd water_6540.pdb ffield.water param.gpu.water
-
-#To run single GPU executable
-puremd-gpu water_6540.pdb ffield.water param.gpu.water
